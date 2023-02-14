c4e2b3bf4799f51f0e867da1e6737fe35995f920
sojinjang <jsojin6119@gmail.com>
2023-02-14 21:24:18 +0900

    :sparkles: feat(asg2): 회원가입 버튼 클릭 시 회원가입 진행

---
+++ src/constants/ApiUrl.js
@@ -0,0 +1,5 @@
+class ApiUrl {
+  static SIGN_UP = "/auth/signup";
+}
+
+module.exports = Object.freeze(ApiUrl);
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { isValidEmail, isValidPw } from "../utils/validator";
+import ApiUrl from "../constants/ApiUrl";
+import { post } from "../utils/api";
 
 const SignUp = () => {
-  const [inputValue, setInputValue] = useState({ email: "", pw: "" });
+  const [inputValue, setInputValue] = useState({ email: "", password: "" });
   const [isEnable, setIsEnable] = useState(false);
-
   const handleInput = (event) => {
     const { name, value } = event.target;
     setInputValue((prev) => {
       return { ...prev, [name]: value };
     });
   };
+  const onClickSignUp = async () => {
+    try {
+      await post(ApiUrl.SIGN_UP, inputValue);
+    } catch (err) {
+      alert(err.message);
+    }
+  };
 
   useEffect(() => {
-    setIsEnable(isValidEmail(inputValue.email) && isValidPw(inputValue.pw));
+    setIsEnable(isValidEmail(inputValue.email) && isValidPw(inputValue.password));
   }, [inputValue]);
 
   return (
@@ -27,13 +35,14 @@ const SignUp = () => {
           data-testid="email-input"
         />
         <input
-          name="pw"
+          name="password"
           onChange={handleInput}
           className="bg-slate-100 m-2 p-2 rounded-lg"
           data-testid="password-input"
         />
         <button
           disabled={!isEnable}
+          onClick={onClickSignUp}
           className="bg-blue-400 m-2 p-2 rounded-lg disabled:opacity-30"
           data-testid="signup-button"
         >
