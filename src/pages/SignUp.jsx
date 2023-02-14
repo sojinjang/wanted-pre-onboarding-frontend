e27fb37d4d836bc7cfcb88daded18d52d2963a24
sojinjang <jsojin6119@gmail.com>
2023-02-14 20:03:47 +0900

    :lipstick: style: 회원가입 컴포넌트 스타일링

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const SignUp = () => {
-  return <>회원가입</>;
+  return (
+    <div className="flex flex-col h-screen items-center justify-center">
+      <h1 className="text-[5vh] p-[5vh]">회원가입</h1>
+      <div className="flex flex-col w-[40vw] text-center">
+        <input className="bg-slate-100 m-2 p-2 rounded-lg" data-testid="email-input" />
+        <input className="bg-slate-100 m-2 p-2 rounded-lg" data-testid="password-input" />
+        <button
+          className="bg-blue-400 m-2 p-2 rounded-lg disabled:opacity-30"
+          data-testid="signup-button"
+        >
+          회원가입
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
