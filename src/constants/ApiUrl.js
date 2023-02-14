ce405f33f55a2a70232b702a71631c247a3103a7
sojinjang <jsojin6119@gmail.com>
2023-02-15 03:53:16 +0900

    :sparkles: feat(asg5): /todo 경로 접속 시 투두 리스트 보여주기

--- src/constants/ApiUrl.js
+++ src/constants/ApiUrl.js
@@ -1,6 +1,7 @@
 class ApiUrl {
   static SIGN_UP = "/auth/signup";
   static SIGN_IN = "/auth/signin";
+  static TODO = "/todos";
 }
 
 module.exports = Object.freeze(ApiUrl);
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const SignIn = () => {
-  return <>로그인</>;
+import { get } from "../utils/api";
+import ApiUrl from "../constants/ApiUrl";
+
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const getTodoData = () => {
+    try {
+      const todoArr = get(ApiUrl.TODO);
+      setTodos(todoArr);
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+  useEffect(() => {
+    getTodoData();
+  }, []);
+
+  return (
+    <div className="flex flex-col h-screen items-center justify-center">
+      {todos &&
+        todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input checked={todo.isCompleted} type="checkbox" />
+                <span>{todo.todo}</span>
+              </label>
+            </li>
+          );
+        })}
+    </div>
+  );
 };
 
-export default SignIn;
+export default Todo;
