758371557e1654c5ef57167f1017bfb59b94ceec
sojinjang <jsojin6119@gmail.com>
2023-02-15 06:25:12 +0900

    :sparkles: feat(asg10): 수정 버튼 클릭 시 수정모드 활성화, 제출/취소로 버튼 교체

--- src/components/todo/SingleTodo.jsx
+++ src/components/todo/SingleTodo.jsx
@@ -4,6 +4,7 @@ import { patch, del } from "../../utils/api";
 import ApiUrl from "../../constants/ApiUrl";
 
 const SingleTodo = ({ todo, todos, setTodos }) => {
+  const [isEditing, setIsEditing] = useState(false);
   const onChangeCheckbox = async (id, isChecked, todo) => {
     try {
       await patch(ApiUrl.TODO, id, { isCompleted: isChecked, todo });
@@ -32,7 +33,7 @@ const SingleTodo = ({ todo, todos, setTodos }) => {
           defaultChecked={todo.isCompleted}
           className="align-middle mr-1 w-[20px] h-[20px]"
         />
-        {false ? (
+        {isEditing ? (
           <input
             value={todo.todo}
             className="bg-slate-100 m-2 p-2 rounded-lg"
@@ -42,10 +43,17 @@ const SingleTodo = ({ todo, todos, setTodos }) => {
           <span className="text-[1.3vh] m-3 align-middle">{todo.todo}</span>
         )}
       </label>
-      {!false && (
+      {isEditing ? (
+        <>
+          <button data-testid="submit-button">제출</button>
+          <button data-testid="cancel-button">취소</button>
+        </>
+      ) : (
         <>
           <button
-            onClick={() => {}}
+            onClick={() => {
+              setIsEditing(true);
+            }}
             className="bg-blue-200 m-2 p-2 rounded-lg"
             data-testid="modify-button"
           >
