fe6ac604b358227b24b004309838ea5bbbd1dd46
sojinjang <jsojin6119@gmail.com>
2023-02-14 21:23:57 +0900

    :sparkles: feat: fetch get, post, patch, delete method들 wrapper 추가

---
+++ src/utils/api.js
@@ -0,0 +1,91 @@
+import { getSavedItem } from "../utils/localStorage";
+import Keys from "../constants/Keys";
+
+async function get(endpointInput, params = "") {
+  const endpoint = process.env.REACT_APP_SERVER_URL + endpointInput;
+  const apiUrl = params === "" ? endpoint : `${endpoint}/${params}`;
+  const res = await fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getSavedItem(Keys.ACCESS_TOKEN)}`,
+    },
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message);
+  }
+
+  const result = res;
+  return result;
+}
+
+async function post(endpointInput, data) {
+  const endpoint = process.env.REACT_APP_SERVER_URL + endpointInput;
+  const apiUrl = endpoint;
+  const bodyData = JSON.stringify(data);
+  const res = await fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getSavedItem(Keys.ACCESS_TOKEN)}`,
+    },
+    body: bodyData,
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message);
+  }
+
+  const result = res;
+  return result;
+}
+
+async function patch(endpointInput, params = "", data) {
+  const endpoint = process.env.REACT_APP_SERVER_URL + endpointInput;
+  const apiUrl = params === "" ? endpoint : `${endpoint}/${params}`;
+  const bodyData = JSON.stringify(data);
+  const res = await fetch(apiUrl, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getSavedItem(Keys.ACCESS_TOKEN)}`,
+    },
+    body: bodyData,
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message);
+  }
+
+  const result = res;
+  return result;
+}
+
+async function del(endpointInput, params = "", data = {}) {
+  const endpoint = process.env.REACT_APP_SERVER_URL + endpointInput;
+  const apiUrl = params === "" ? endpoint : `${endpoint}/${params}`;
+  const bodyData = JSON.stringify(data);
+
+  const res = await fetch(apiUrl, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getSavedItem(Keys.ACCESS_TOKEN)}`,
+    },
+    body: bodyData,
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message);
+  }
+
+  const result = res;
+  return result;
+}
+
+export { get, post, patch, del as delete };
