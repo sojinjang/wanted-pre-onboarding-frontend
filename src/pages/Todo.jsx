7dca458af21c94794f8f996080d5dda63be5bf2b
sojinjang <jsojin6119@gmail.com>
2023-02-15 04:59:39 +0900

    :sparkles: feat(asg6): todo 입력창과 버튼 추가, 버튼 클릭 시 새로운 todo 추가

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,28 +1,59 @@
 import React, { useState, useEffect } from "react";
 
-import { get } from "../utils/api";
+import { get, post } from "../utils/api";
 import ApiUrl from "../constants/ApiUrl";
 
 const Todo = () => {
   const [todos, setTodos] = useState([]);
-  const getTodoData = () => {
+  const [todoInput, setTodoInput] = useState("");
+  const handleTodoInput = (event) => {
+    setTodoInput(event.target.value);
+  };
+  const getTodoData = async () => {
     try {
-      const todoArr = get(ApiUrl.TODO);
+      const todoArr = await get(ApiUrl.TODO);
       setTodos(todoArr);
     } catch (err) {
       alert(err.message);
     }
   };
+  const postTodoData = async () => {
+    try {
+      const newTodo = await post(ApiUrl.TODO, { todo: todoInput });
+      setTodos((prev) => {
+        return [...prev, newTodo];
+      });
+      setTodoInput("");
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
   useEffect(() => {
     getTodoData();
   }, []);
 
   return (
     <div className="flex flex-col h-screen items-center justify-center">
+      <div className="flex">
+        <input
+          value={todoInput}
+          onChange={handleTodoInput}
+          className="bg-slate-100 m-2 p-2 rounded-lg"
+          data-testid="new-todo-input"
+        />
+        <button
+          onClick={postTodoData}
+          className="bg-blue-400 m-2 p-2 rounded-lg"
+          data-testid="new-todo-add-button"
+        >
+          추가
+        </button>
+      </div>
       {todos &&
         todos.map((todo) => {
           return (
-            <li key={todo.id}>
+            <li className="text-[2vh]" key={todo.id}>
               <label className="inline-block whitespace-nowrap">
                 <input className="align-middle" checked={todo.isCompleted} type="checkbox" />
                 <span className="ml-2 align-middle">{todo.todo}</span>
