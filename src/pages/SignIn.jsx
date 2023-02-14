7b4e8466e5fd0e0e3388d1aee0c550e609e29d77
sojinjang <jsojin6119@gmail.com>
2023-02-14 21:58:22 +0900

    :sparkles: feat(asg1): 로그인 페이지 입력값 유효성 검사 기능 결과에 따라 버튼 속성 변경 기능 추가

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { isValidEmail, isValidPw } from "../utils/validator";
+import { post } from "../utils/api";
+import { saveItem } from "../utils/localStorage";
+import ApiUrl from "../constants/ApiUrl";
+import Keys from "../constants/Keys";
+import { PRIVATE_ROUTE } from "../router/ROUTE_INFO";
 
 const SignIn = () => {
-  return <>로그인</>;
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({ email: "", password: "" });
+  const [isEnable, setIsEnable] = useState(false);
+  const handleInput = (event) => {
+    const { name, value } = event.target;
+    setInputValue((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+  const onClickSignIn = async () => {
+    try {
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  useEffect(() => {
+    setIsEnable(isValidEmail(inputValue.email) && isValidPw(inputValue.password));
+  }, [inputValue]);
+
+  return (
+    <div className="flex flex-col h-screen items-center justify-center">
+      <h1 className="text-[5vh] p-[5vh]">로그인</h1>
+      <div className="flex flex-col w-[40vw] text-center">
+        <input
+          name="email"
+          onChange={handleInput}
+          className="bg-slate-100 m-2 p-2 rounded-lg"
+          data-testid="email-input"
+        />
+        <input
+          name="password"
+          onChange={handleInput}
+          className="bg-slate-100 m-2 p-2 rounded-lg"
+          data-testid="password-input"
+        />
+        <button
+          disabled={!isEnable}
+          onClick={onClickSignIn}
+          className="bg-blue-400 m-2 p-2 rounded-lg disabled:opacity-30"
+          data-testid="signin-button"
+        >
+          로그인
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
